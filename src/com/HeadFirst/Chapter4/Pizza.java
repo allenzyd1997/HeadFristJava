4b0c639e8b4374ea5bca64d2c35c763ff0b3a884
allenzyd1997 <allenwzyd@icloud.com>
2020-03-08 23:43:57 +0800

    创建第四章基础类

---
+++ src/com/HeadFirst/Chapter4/Pizza.java
@@ -0,0 +1,23 @@
+package com.HeadFirst.Chapter4;
+
+public class Pizza {
+
+    String pizza ;
+
+    public Pizza(String pizza) {
+        this.pizza = pizza;
+    }
+
+    public void bake() {
+    }
+
+    public void prepare() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
+
+}
---
+++ src/com/HeadFirst/Chapter4/PizzaStore.java
@@ -0,0 +1,16 @@
+package com.HeadFirst.Chapter4;
+
+public abstract class PizzaStore {
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+        pizza = createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    abstract Pizza createPizza(String type);
+}
