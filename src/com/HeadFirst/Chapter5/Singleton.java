be963c027492dfb11d2109300339512bf09dafc0
allenzyd1997 <allenwzyd@icloud.com>
2020-03-17 22:14:31 +0800

    添加并行设计

--- src/com/HeadFirst/Chapter5/Singleton.java
+++ src/com/HeadFirst/Chapter5/Singleton.java
@@ -1,4 +1,15 @@
 package com.HeadFirst.Chapter5;
 
 public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton(){}
+
+    public static synchronized Singleton getInstance(){
+        if (uniqueInstance == null){
+            uniqueInstance = new Singleton();
+
+        }
+        return uniqueInstance;
+    }
 }
