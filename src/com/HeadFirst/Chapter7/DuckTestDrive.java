878715d0e5918aaebc466201d2003d798458b9fd
allenzyd1997 <allenwzyd@icloud.com>
2020-03-25 22:23:17 +0800

    补充适配器部分代码和readme

---
+++ src/com/HeadFirst/Chapter7/DuckTestDrive.java
@@ -0,0 +1,27 @@
+package com.HeadFirst.Chapter7;
+
+public class DuckTestDrive {
+
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck();
+
+        WildTurkey turkey = new WildTurkey();
+
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("The Turkey says : ");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\n The Duck says : ");
+        testDuck(duck);
+
+        System.out.println("\n The Turkey Adapter says : ");
+        testDuck(turkeyAdapter);
+    }
+
+    static void testDuck(Duck duck){
+        duck.quack();
+        duck.fly();
+    }
+}
